34e72d5200a67b9b5a225a28b920426214a08100
user <myriam.benali3@gmail.com>
2019-06-17 00:10:21 +0200

    Fin du projet

--- db/commentaire.sql
+++
@@ -1,5 +0,0 @@
-use formaweb2
-insert into commentaire values
-(1,'Merci beaucoup pour cette excellente conférence', 1,1);
-insert into commentaire values
-(2,'Une conférence vraiment très instructive.', 1,2);
